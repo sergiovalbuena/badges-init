57b8eb3a42c5d049cb922f3ed672693baa6e9fb0
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-06-24 18:23:34 -0500

    styles added

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
+import "./styles/Badge.css";
 import imgHeader from '../imgs/badge-header.svg';
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className="Bagde">
+                <div className="Badge__header">
                     <img src={imgHeader} alt="logo"></img>
                 </div>
 
-                <div>
-                <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+                <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
                     <h1>Sergio <br></br>Valbuena</h1>
                 </div>
-                <div>
-                    <p>Frontend Designer and Developer</p>
-                    <p>@twitter.name</p>
+                <div className="Badge__section-info">
+                    <h3>Frontend Designer and Developer</h3>
+                    <div>@twitter.name</div>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './global.css'
 import Badge from './components/Badge'
+
+
 
 const container = document.getElementById('app');
 
