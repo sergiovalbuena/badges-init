884ee4434f8f4da32fc281579956a9b73e04a73e
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-06-25 11:24:35 -0500

    enviando datos al form con state

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+    state = {
+        //inincalizar un state 
+        //jobTitle: 'Designer', OUTPUT: Designer es el valor por default en el form en el espacio jobTitle
+
+    };
     handleChange = e => {
-        console.log({
-            name: e.target.name,
-            value: e.target.value,
-        });
+        // console.log({
+        //     name: e.target.name,
+        //     value: e.target.value,
+      this.setState({
+        [e.target.name]: e.target.value,
+      })  
     };
     handleClick = e => {
         console.log("Button was clicked")
@@ -13,6 +20,7 @@ class BadgeForm extends React.Component {
     handleSubmit = e => {
         e.preventDefault();
         console.log("Form was submited")
+        console.log(this.state);
     };
     
     render(){
@@ -22,7 +30,53 @@ class BadgeForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName"></input>
+                        <input 
+                        onChange={this.handleChange} className="form-control" 
+                        type="text" 
+                        name="firstName"
+                        value={this.state.firstName}
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input 
+                        onChange={this.handleChange} className="form-control" 
+                        type="text" 
+                        name="lastName"
+                        value={this.state.lastName}
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>e-mail</label>
+                        <input 
+                        onChange={this.handleChange} className="form-control" 
+                        type="email" 
+                        name="email"
+                        value={this.state.email}
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input 
+                        onChange={this.handleChange} className="form-control" 
+                        type="text" 
+                        name="jobTitle"
+                        value={this.state.jobTitle}
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input 
+                        onChange={this.handleChange} className="form-control" 
+                        type="text" 
+                        name="twitter"
+                        value={this.state.twitter}
+                        >
+                        </input>
                     </div>
 
                     <button onClick={this.handleClick} className="btn btn-primary">ENVIAR</button>
